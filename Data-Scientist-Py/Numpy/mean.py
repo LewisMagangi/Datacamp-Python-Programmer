89b887ea214e6a5c2e34d3561ab7d534e6345d31
Liquelaliqour <lewismomanyi34@gmail.com>
2023-01-22 05:36:53 -0800

    numpy stastistics

---
+++ Data-Scientist-Py/Numpy/mean.py
@@ -0,0 +1,9 @@
+# Python Program illustrating
+# numpy.mean() method
+import numpy as np
+
+# 1D array
+array = [201, 298, 789, 891, 374]
+
+print("arr : ", array)
+print("mean of arr : ", np.mean(array))
