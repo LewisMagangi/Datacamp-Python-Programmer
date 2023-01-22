6227cb710d0057f1f3e74dacfbbe780fb6a276ba
Liquelaliqour <lewismomanyi34@gmail.com>
2023-01-22 05:25:31 -0800

    numpy arithmetic

---
+++ Data-Scientist-Py/Numpy/arithmetic.py
@@ -0,0 +1,11 @@
+import numpy as np
+np_mat = np.array([[1, 2],
+                   [3, 4],
+                   [5, 6]])
+x = np_mat * 4
+y = np_mat + np.array([10, 10])
+z = np_mat + np_mat
+
+print(x)
+print(y)
+print(z)
