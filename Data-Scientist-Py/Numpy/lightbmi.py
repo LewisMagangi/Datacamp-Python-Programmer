061f4f7ca4199c2ed377266238ea635d688dba00
Liquelaliqour <lewismomanyi34@gmail.com>
2023-01-22 05:09:26 -0800

    slcing using numpy

--- Data-Scientist-Py/Numpy/lightbmi.py
+++ Data-Scientist-Py/Numpy/lightbmi.py
@@ -1,4 +1,4 @@
-mmmx# height_inch and weight_lb are available as regular lists
+# height_inch and weight_lb are available as regular lists
 height_inch = [80, 70, 65, 72, 88, 76, 93, 59]
 weight_lb = [180, 170, 165, 172, 188, 176, 193, 159]
 
@@ -11,8 +11,14 @@ np_height_m = np.array(height_inch) * 0.0254
 # Create array from weight_lb with metric units: np_weight_kg
 np_weight_kg = np.array(weight_lb) * 0.453592
 
-# Calculate the BMI: bmi
+#bmi
 bmi = np_weight_kg / np_height_m ** 2
 
-# Print out bmi
-print(bmi)
+# Create the light array
+light = np.array(bmi < 21)
+
+# Print out light
+print(light)
+
+# Print out BMIs of all baseball players whose BMI is below 21
+print(bmi[light])
